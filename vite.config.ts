b131f98f880320807720be8df80f6faba3ba934d
gakcreations <gakcreationsx@gmail.com>
2026-07-17 16:55:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "",
-  plugins: [
-    tanstackRouter({ target: "react" }),
-    react(),
-    tailwindcss(),
-    tsconfigPaths()
-  ],
-});
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
+})
